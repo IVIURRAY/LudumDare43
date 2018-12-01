fa008d7adfd8018194c79275ae79bf2d39fc500d
HAM <mini_muz_11@hotmail.co.uk>
2018-12-01 09:53:07 +0000

    Player can move forward back left and right

---
+++ LudumDare43/Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 5f;
+
+	private PlayerMovement movement;
+
+	private void Start()
+	{
+		movement = GetComponent<PlayerMovement>();
+	}
+
+	private void Update()
+	{
+		// Calculate movement as a 3D vector
+		float xMov = Input.GetAxisRaw("Horizontal");
+		float yMov = Input.GetAxisRaw("Vertical");
+
+		Vector3 movHorizontal = transform.right * xMov;
+		Vector3 movVertical = transform.forward * yMov;
+
+		// Final movement vetor
+		Vector3 velocity = (movHorizontal + movVertical).normalized * speed;
+
+		// Apply movement
+		movement.Move(velocity);
+
+	}
+
+}
---
+++ LudumDare43/Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour {
+
+	private Vector3 velocity = Vector3.zero;
+
+	private Rigidbody rb;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Gets the movement vector
+	public void Move(Vector3 inputVelocity)
+	{
+		velocity = inputVelocity;
+	}
+
+	// Run every physics iteration
+	private void FixedUpdate()
+	{
+		PerformMovement();
+	}
+
+	private void PerformMovement()
+	{
+		if (velocity != Vector3.zero)
+		{
+			rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+		}
+	}
+
+}

6b81c89370b0b183fbac951b2e37955a21ade1de
HAM <mini_muz_11@hotmail.co.uk>
2018-12-01 16:14:37 +0000

    Adding candle light

--- LudumDare43/Assets/Scripts/Pickups/PickupTourch.cs
+++ LudumDare43/Assets/Scripts/Pickups/PickupTourch.cs
@@ -4,29 +4,20 @@ using UnityEngine;
 
 public class PickupTourch : Pickupable {
 
-	
+	GameObject tourch;
 
 	// Use this for initialization
 	void Start () {
-		
+		tourch = GameObject.FindWithTag("TourchModel");
+		tourch.GetComponent<Light>().enabled = false;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
 	public override void Run()
 	{
 		Debug.Log("i am tourch");
 
-		// GameObject tourch = player.transform.Find("TourchModel").gameObject;
-		GameObject.FindWithTag("TourchModel").GetComponent<MeshRenderer>().enabled = true;
-		
-		
-
-		// Debug.Log(tourch);
-
+		tourch.GetComponent<MeshRenderer>().enabled = true;
+		tourch.GetComponent<Light>().enabled = true;		
 		Destroy(gameObject);
 	}
 }

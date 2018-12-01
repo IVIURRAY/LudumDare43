8702809b7613eebd6833f2f6f2976459f1862c9b
HAM <mini_muz_11@hotmail.co.uk>
2018-12-01 14:53:27 +0000

    add post processing stack

---
+++ LudumDare43/Assets/Scripts/Pickups/PickupLSD.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupLSD : Pickupable {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void Run()
+	{
+		Debug.Log("Im LSD");
+	}
+
+}

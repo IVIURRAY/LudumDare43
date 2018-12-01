cc018f2cd68ec727d9e6ee58b738bf7673b623f9
HAM <mini_muz_11@hotmail.co.uk>
2018-12-01 13:01:35 +0000

    making pickupables run on awake

--- LudumDare43/Assets/Scripts/Pickupable.cs
+++ LudumDare43/Assets/Scripts/Pickupable.cs
@@ -6,7 +6,7 @@ public class Pickupable : MonoBehaviour {
 
 	private Dictionary<string, Pickupable> pickupScripts = new Dictionary<string, Pickupable>();
 
-	private void Start()
+	private void Awake()
 	{
 		PickupTourch p_touch = (PickupTourch)FindObjectOfType(typeof(PickupTourch));
 		Debug.Log(p_touch);

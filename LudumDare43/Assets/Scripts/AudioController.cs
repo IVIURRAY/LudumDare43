7ccaaad0ec4967fc9ec93f7bf4a03d74145c8853
HAM <mini_muz_11@hotmail.co.uk>
2018-12-02 11:14:59 +0000

    addding audio

---
+++ LudumDare43/Assets/Scripts/AudioController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+	private void Awake()
+	{
+		if (FindObjectsOfType<AudioController>().Length > 1)
+			Destroy(this.gameObject);
+
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
